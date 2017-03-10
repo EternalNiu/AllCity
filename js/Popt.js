1608899163cefbbeeaeb4766b3016bbc1c6b2d2c
Niu Qichao <1027089889@qq.com>
2017-03-10 14:35:58 +0800

    change

---
+++ js/Popt.js
@@ -0,0 +1,233 @@
+﻿Array.prototype.unique = function () {//去数组重复
+    return this.sort().join(",,").replace(/(,|^)([^,]+)(,,\2)+(,|$)/g, "$1$2$4").replace(/,,+/g, ",").replace(/,$/, "").split(",");
+}
+var Iput_one = {
+    confg: {
+        hand: "0", //0对像位置1鼠标位置divID滚动位置
+        idIframe: "PoPx", //默认可不用改
+        idBox: "PoPy", //默认可不用改
+        content: "", //传过来的内容
+        ok: null, //弹出框之后执行的函数
+        id: null, //不能为空一般传this对像而不是对像ID
+        event: window.event, //这个必写一般为e就可以了
+        top: 0, //顶部偏移位置
+        left: 0, //左部偏移位置
+        bodyHeight: 0, //在被position:absolute元素下得到HTML真实高度
+        bodyWidth: 0,
+        width: 0,
+        soll: null,
+        pop: null //指定ID点击时不关闭
+    },
+    get: function (obj) { return document.getElementById(obj); },
+    lft: function (e) {
+        var l = 0;
+        while (e) { l += e.offsetLeft; e = e.offsetParent; }
+        return l
+    },
+    ltp: function (e) {
+        var t = 0;
+        while (e) { t += e.offsetTop; e = e.offsetParent; }
+        return t
+    },
+    clear: function () {
+        Iput_one.confg.hand = "0"; Iput_one.confg.ok = null; Iput_one.confg.top = 0; Iput_one.confg.left = 0; Iput_one.confg.bodyHeight = 0; Iput_one.confg.bodyWidth = 0; Iput_one.confg.width = 0; Iput_one.confg.pop = null;
+    },
+    stopBubble: function (e) {
+        if (e && e.stopPropagation) {
+            e.stopPropagation();    //w3c
+        } else {
+            window.event.cancelBubble = true; //IE
+        }
+    },
+    pop: function () {
+        var $a = document.getElementsByTagName("body").item(0);
+        var $c = document.createElement("iframe");
+        var $b = document.createElement("div");
+        $c.setAttribute('id', Iput_one.confg.idIframe);
+        $c.setAttribute("src", "about:blank");
+        $c.style.zindex = '100';
+        $c.frameBorder = "0";
+        $c.style.width = "0px";
+        $c.style.height = "0px";
+        $c.style.position = 'absolute';
+        $b.setAttribute('id', Iput_one.confg.idBox);
+        $b.setAttribute('align', 'left');
+        $b.style.position = 'absolute';
+        $b.style.background = 'transparent';
+        $b.style.zIndex = '20000';
+        if ($a) {
+            if (Iput_one.get(Iput_one.confg.idIframe)) {
+                Iput_one.colse();
+            }
+            $a.appendChild($c);
+            if ($c) {
+                $c.ownerDocument.body.appendChild($b);
+            }
+            Iput_one.get(Iput_one.confg.idBox).innerHTML = Iput_one.confg.content;
+            Iput_one.drice(Iput_one.confg.event);
+        }
+
+        if (!document.all) {
+            window.document.addEventListener("click", Iput_one.hide, false);
+        }
+        else {
+            window.document.attachEvent("onclick", Iput_one.hide);
+        }
+    },
+    drice: function (e) {
+        var bodyHith = Iput_one.confg.bodyHeight == 0 ? document.body.scrollHeight : Iput_one.confg.bodyHeight;
+        var bodywidth = Iput_one.confg.bodyWidth == 0 ? document.body.scrollWidth : Iput_one.confg.bodyWidth;
+        if (!e) e = window.event;
+        var top = 0, left = 0;
+        var a = Iput_one.get(Iput_one.confg.idBox);
+        var b = Iput_one.get(Iput_one.confg.idIframe);
+        var c = Iput_one.confg.id.offsetHeight;
+        var d = Iput_one.confg.id.offsetWidth;
+        var w = 0;
+        var st = 0;
+        var sl = 0;
+        if (Iput_one.confg.soll != null) {
+            st = document.getElementById(Iput_one.confg.soll).scrollTop;
+            sl = document.getElementById(Iput_one.confg.soll).scrollLeft;
+        }
+        if (Iput_one.get(Iput_one.confg.idIframe)) {
+            if (Iput_one.confg.hand == "1") {
+                top = Iput_one.confg.top + document.body.scrollTop + document.documentElement.scrollTop + e.clientY;
+                left = Iput_one.confg.left + e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                if (a.offsetHeight + top > bodyHith) { top = top - a.offsetHeight + Iput_one.get(Iput_one.confg.idBox).firstChild.offsetHeight; }
+                if (a.offsetWidth + left > bodywidth) { left = left - a.offsetWidth + Iput_one.get(Iput_one.confg.idBox).firstChild.offsetWidth; }
+                a.style.top = top - st + "px";
+                b.style.top = top - st + "px";
+                a.style.left = left - sl + "px";
+                b.style.left = left - sl + "px";
+            }
+            else if (Iput_one.confg.hand == "0") {
+                w = Iput_one.confg.id.offsetWidth + "px";
+                a.style.width = w;
+                b.style.width = w;
+                height = c;
+                top = Iput_one.confg.top + Iput_one.ltp(Iput_one.confg.id);
+                left = Iput_one.confg.left + Iput_one.lft(Iput_one.confg.id);
+                if (a.firstChild.offsetHeight + top + c > bodyHith) { top = top - a.firstChild.offsetHeight - c; }
+                if (a.firstChild.offsetWidth + left > bodywidth) { left = left - a.firstChild.offsetWidth + d; }
+                b.style.top = top - st + "px";
+                a.style.top = top - st + height + "px";
+                b.style.left = left - sl + "px";
+                a.style.left = left - sl + "px";
+            }
+            else {
+                height = c;
+                top = Iput_one.confg.top - Iput_one.get(Iput_one.confg.hand).scrollTop + Iput_one.ltp(Iput_one.confg.id);
+                left = Iput_one.confg.left - Iput_one.get(Iput_one.confg.hand).scrollLeft + Iput_one.lft(Iput_one.confg.id);
+
+                if (a.offsetHeight + top > bodyHith) { top = top - a.offsetHeight - c; }
+                if (a.offsetWidth + left > bodywidth) { left = left - a.offsetWidth - d; }
+
+                b.style.top = top - st + height + "px";
+                a.style.top = top - st + height + "px";
+                b.style.left = left - sl + "px";
+                a.style.left = left - sl + "px";
+            }
+        }
+    },
+    show: function () {
+        var config = arguments[0]; var that = Iput_one.confg;
+        Iput_one.clear();
+        for (var i in that) { if (config[i] != undefined) { that[i] = config[i]; } };
+        Iput_one.pop();
+        if (Iput_one.confg.ok != null) {
+            Iput_one.action(Iput_one.confg.ok());
+        }
+    },
+    colse: function () {
+        if (Iput_one.get(Iput_one.confg.idIframe)) {
+            document.body.removeChild(Iput_one.get(Iput_one.confg.idBox));
+            document.body.removeChild(Iput_one.get(Iput_one.confg.idIframe));
+        }
+        if (Iput_one.get(Iput_one.confg.pop)) {
+            Iput_one.get(Iput_one.confg.pop).style.display = "none";
+        }
+    },
+    $colse: function () { Iput_one.colse(); },
+    hide: function (e) {//点击任何处关闭层
+        e = window.event || e;
+        var srcElement = e.srcElement || e.target;
+        if (Iput_one.confg.event == undefined) {//输入时用,般在没传入Iput_one.confg.event请况下使用
+            Iput_one.colse();
+        }
+        else {
+            var a = Iput_one.confg.event.srcElement || Iput_one.confg.event.target;
+            var b = Iput_one.get(Iput_one.confg.pop);
+            if (a != srcElement) { Iput_one.colse(); }
+            if (b != null) {
+                if (b != srcElement && a != srcElement) { Iput_one.colse(); }
+            }
+        }
+        if (Iput_one.get(Iput_one.confg.idIframe)) {
+            Iput_one.get(Iput_one.confg.idIframe).onclick = function (e) { Iput_one.stopBubble(e); };
+            Iput_one.get(Iput_one.confg.idBox).onclick = function (e) { Iput_one.stopBubble(e); };
+        }
+        if (Iput_one.get(Iput_one.confg.pop)) {
+            Iput_one.get(Iput_one.confg.pop).onclick = function (e) { Iput_one.stopBubble(e); };
+        }
+
+    },
+    action: function (obj) {
+        eval(obj);
+    },
+    cookie: {
+        Set: function (name, val) {
+            var Days = 30;                          //此 cookie 将被保存 30 天
+            var exp = new Date();                  //new Date("December 31, 9998");
+            exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+            document.cookie = name + "=" + escape(val) + ";expires=" + exp.toGMTString() + "; path=/";
+        },
+        Get: function (name) {
+            var start = document.cookie.indexOf(name);
+            var end = document.cookie.indexOf(";", start);
+            return start == -1 ? null : unescape(document.cookie.substring(start + name.length + 1, (end > start ? end : document.cookie.length)));
+        },
+        Del: function (name) {
+            var exp = new Date();
+            exp.setTime(exp.getTime() - 1);
+            var cval = this.GetCookie(name);
+            if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+        }
+    },
+    ischeck: function (bol) {
+        var objs = form1.getElementsByTagName("input");
+        if (bol) {
+            for (var i = 0; i < objs.length; i++) { if (objs[i].type.toLowerCase() == "checkbox") { objs[i].checked = true; } }
+        }
+        else {
+            for (var i = 0; i < objs.length; i++) { if (objs[i].type.toLowerCase() == "checkbox") { objs[i].checked = false; } }
+        }
+    },
+    contains: function (star, end, isIgnoreCase) {
+        if (isIgnoreCase) {
+            star = star.toLowerCase();
+            end = end.toLowerCase();
+        }
+        var startChar = end.substring(0, 1);
+        var strLen = end.length;
+        for (var j = 0; j < star.length - strLen + 1; j++) {
+            if (star.charAt(j) == startChar)//如果匹配起始字符,开始查找
+            {
+                if (star.substring(j, j + strLen) == end)//如果从j开始的字符与str匹配，那ok
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    gData: function (name, value) {
+        var top = window.top, cache = top['_CACHE'] || {};
+        top['_CACHE'] = cache;
+        return value ? cache[name] = value : cache[name];
+    },
+    rData: function (name) {
+        var cache = window.top['_CACHE'];
+        if (cache && cache[name]) delete cache[name];
+    }
+}
---
+++ js/workSet.js
@@ -0,0 +1,143 @@
+function SelCityWork(obj,e) {
+    var ths = obj;
+    var dal = '<div class="_citys_one"><span title="关闭" id="cColse_one" >×</span><ul id="_citysheng_one" class="_citys0_one"><li class="citySel_one  check_one" >请选择</li><li class="check_two">请选择</li></ul><div id="_citys0_one" class="_citys1_one"></div><div style="display:none" id="_citys1_one" class="_citys1_one"></div><div style="display:none" id="_citys2_one" class="_citys1_one"></div></div>';
+    Iput_one.show({ id: ths, event: e, content: dal,width:"470"});
+    $("#cColse_one").click(function () {
+        Iput_one.colse();
+    });
+    var tb_province = [];
+    var b = work;
+    for (var i = 0, len = b.length; i < len; i++) {
+        tb_province.push('<a data-level="0" data-id="' + b[i]['id'] + '" data-name="' + b[i]['name'] + '">' + b[i]['name'] + '</a>');
+    }
+    $("#_citys0_one").append(tb_province.join(""));
+    $("#_citys0_one a").click(function () {
+        var g = getCity($(this));
+        $("#_citys1_one a").remove();
+        $("#_citys1_one").append(g);
+        $("._citys1_one").hide();
+        $("._citys1_one:eq(1)").show();
+        $("#_citys0_one a,#_citys1_one a,#_citys2_one a").removeClass("AreaS_one");
+        $(this).addClass("AreaS_one");
+        var lev = $(this).data("name");
+        ths.value = $(this).data("name");
+        $('.check_one').text($(this).text());
+        if (document.getElementById("hcity_one") == null) {
+            var hcitys = $('<input>', {
+                type: 'hidden',
+                name: "hcity",
+                "data-id": $(this).data("id"),
+                id: "hcity_one",
+                val: lev
+            });
+            $(ths).after(hcitys);
+        }
+        else {
+            $("#hcity_one").val(lev);
+            $("#hcity_one").attr("data-id", $(this).data("id"));
+        }
+        $("#_citys1_one a").click(function () {
+            $("#_citys1_one a,#_citys2_one a").removeClass("AreaS_one");
+            $(this).addClass("AreaS_one");
+            var lev =  $(this).data("name");
+            if (document.getElementById("hproper_one") == null) {
+                var hcitys = $('<input>', {
+                    type: 'hidden',
+                    name: "hproper",
+                    "data-id": $(this).data("id"),
+                    id: "hproper_one",
+                    val: lev
+                });
+                $(ths).after(hcitys);
+            }
+            else {
+                $("#hproper_one").attr("data-id", $(this).data("id"));
+                $("#hproper_one").val(lev);
+            }
+            var bc = $("#hcity_one").val();
+            ths.value = bc + $(this).data("name");
+            // var text = $('#city_one').val();
+            // if(text.length > 12){
+            //     text = text.substring(0,12)+ "...";
+            // }
+            // $('.name_work').html(text);
+            Iput_one.colse();
+            // var ar = getArea($(this));
+
+            // $("#_citys2_one a").remove();
+            // $("#_citys2_one").append(ar);
+            // $("._citys1_one").hide();
+            // $("._citys1_one:eq(2)").show();
+
+
+            // $("#_citys2_one a").click(function () {
+            //     $("#_citys2_one a").removeClass("AreaS_one");
+            //     $(this).addClass("AreaS_one");
+            //     var lev = $(this).data("name");
+            //     if (document.getElementById("harea_one") == null) {
+            //         var hcitys = $('<input>', {
+            //             type: 'hidden',
+            //             name: "harea",
+            //             "data-id": $(this).data("id"),
+            //             id: "harea_one",
+            //             val: lev
+            //         });
+            //         $(ths).after(hcitys);
+            //     }
+            //     else {
+            //         $("#harea_one").val(lev);
+            //         $("#harea_one").attr("data-id", $(this).data("id"));
+            //     }
+            //     var bc = $("#hcity_one").val();
+            //     var bp = $("#hproper_one").val();
+            //     ths.value = bc + bp + $(this).data("name");
+
+            // });
+
+        });
+    });
+    $("#_citysheng_one li").click(function () {
+        $("#_citysheng_one li").removeClass("citySel_one");
+        $(this).addClass("citySel_one");
+        var s = $("#_citysheng_one li").index(this);
+        $("._citys1_one").hide();
+        $("._citys1_one:eq(" + s + ")").show();
+    });
+}
+
+function getCity(obj) {
+    var c = obj.data('id');
+    var e = work;
+    var f;
+    var g = '';
+    for (var i = 0, plen = e.length; i < plen; i++) {
+        if (e[i]['id'] == parseInt(c)) {
+            f = e[i]['city'];
+            break
+        }
+    }
+    for (var j = 0, clen = f.length; j < clen; j++) {
+        g += '<a data-level="1" data-id="' + f[j]['id'] + '" data-name="' + f[j]['name'] + '" title="' + f[j]['name'] + '">' + f[j]['name'] + '</a>'
+    }
+    $("#_citysheng_one li").removeClass("citySel_one");
+    $("#_citysheng_one li:eq(1)").addClass("citySel_one");
+    return g;
+}
+function getArea(obj) {
+    var c = obj.data('id');
+    var e = area;
+    var f = [];
+    var g = '';
+    for (var i = 0, plen = e.length; i < plen; i++) {
+        if (e[i]['pid'] == parseInt(c)) {
+            f.push(e[i]);
+        }
+    }
+    for (var j = 0, clen = f.length; j < clen; j++) {
+        g += '<a data-level="1" data-id="' + f[j]['id'] + '" data-name="' + f[j]['name'] + '" title="' + f[j]['name'] + '">' + f[j]['name'] + '</a>'
+    }
+
+    $("#_citysheng_one li").removeClass("citySel_one");
+    $("#_citysheng_one li:eq(2)").addClass("citySel_one");
+    return g;
+}
